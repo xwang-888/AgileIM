a701959f149852db5b14ac941dfda166eada9c1b
轩 王 <15991346603@163.com>
2022-04-20 01:17:42 +0800

    日常提交

--- AgileIM.Client/Controls/AccountBox.cs
+++ AgileIM.Client/Controls/AccountBox.cs
@@ -7,11 +7,22 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
+using AgileIM.Client.Models;
+
 namespace AgileIM.Client.Controls
 {
     [StyleTypedProperty(Property = "ItemContainerStyle", StyleTargetType = typeof(ComboBoxItem))]
-    public class AccountBox: Selector
+    [TemplatePart(Name = PARK_ArrowToggleButton, Type = typeof(ToggleButton))]
+    [TemplatePart(Name = PARK_ListView, Type = typeof(ListView))]
+    public class AccountBox : Selector
     {
+
+
+        private const string PARK_ArrowToggleButton = "ToggleButton";
+        private const string PARK_ListView = "ListView";
+        private ToggleButton ArrowToggleButton;
+        private ListView ListView;
+
         public static readonly DependencyProperty AccountProperty = DependencyProperty.Register(
             "Account", typeof(string), typeof(AccountBox), new PropertyMetadata(default(string)));
 
@@ -19,6 +30,34 @@ namespace AgileIM.Client.Controls
         {
             get => (string)GetValue(AccountProperty);
             set => SetValue(AccountProperty, value);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            ArrowToggleButton = GetTemplateChild(PARK_ArrowToggleButton) as ToggleButton;
+            ListView = GetTemplateChild(PARK_ListView) as ListView;
+            ArrowToggleButton.Click += ArrowToggleButton_Click;
+            ListView.SelectionChanged += ListView_SelectionChanged;
+            base.OnApplyTemplate();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ArrowToggleButton.IsChecked = false;
+            isOpen = false;
+        }
+
+        private bool isOpen = false;
+        private void ArrowToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isOpen)
+            {
+                ArrowToggleButton.IsChecked = false;
+                isOpen = false;
+            }
+            else
+                isOpen = true;
+
         }
     }
 }
